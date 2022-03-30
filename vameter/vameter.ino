f614b478d7a31dd269f588bfc3bef657644d7ca5
agnestachyon <yuu.hori.15.17@gmail.com>
2022-03-31 05:01:57 +0900

    見様見真似で

--- vameter/vameter.ino
+++ vameter/vameter.ino
@@ -3,18 +3,38 @@
 #define address //センサのアドレス
 
 int sensdata; //センサから受け取ったデータを格納
+int current;
+int voltage;
 
 void setup(){
   Wire.begin();
   Serial.begin(115200);
 }
 
-void loop(){ //通信・デバイスの固有ID確認用
-  Wire.beginTransmission(address); //通信の開始。slave側のアドレスを定義する
-  Wire.write(0x00); //固有IDの格納場所のアドレス
-  Wire.endTransmittion(); //送信の実行
-  Wire.requestFrom(address, 1); //slaveアドレスを再定義。レジスタアドレスから1アドレスのデータを読み込む
-  sensdata = Wire.read();
-  Serial.println(sensdata);
-  delay(500)
+// void loop(){ //通信・デバイスの固有ID確認用
+//   Wire.beginTransmission(address); //通信の開始。slave側のアドレスを定義する
+//   Wire.endTransmittion(); //送信の実行
+//   Wire.requestFrom(address, 1); //slaveアドレスを再定義。レジスタアドレスから1バイトのデータを読み込む
+//   sensdata = Wire.read();
+//   Serial.println(sensdata);
+//   delay(500)
+// }
+
+void loop(){
+  Wire.beginTransmission(address); //電流データの焼き鳥
+  Wire.write(0x07);
+  Wire.endTransmittion();
+  Wire.requestFrom(address, 1);
+  current = Wire.read();
+
+  Wire.beginTransmission(address); //電圧データの焼き鳥
+  Wire.write(0x04);
+  Wire.endTransmittion();
+  Wire.requestFrom(address, 1);
+  voltage = Wire.read();
+
+  Serial.print(current);
+  Serial.println(voltage);
+  
+  delay(500);
 }
