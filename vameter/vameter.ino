9853d08b595899342f64167d4445c1e2a44fe589
agnestachyon <yuu.hori.15.17@gmail.com>
2022-06-04 08:44:02 +0900

    アドレス設定

--- vameter/vameter.ino
+++ vameter/vameter.ino
@@ -1,6 +1,5 @@
 #include <Wire.h> //I2C取り扱うためのライブラリ
-
-#define address //センサのアドレス
+#define address 64//センサのアドレス
 
 int sensdata; //センサから受け取ったデータを格納
 int current;
@@ -13,27 +12,27 @@ void setup(){
 
 // void loop(){ //通信・デバイスの固有ID確認用
 //   Wire.beginTransmission(address); //通信の開始。slave側のアドレスを定義する
-//   Wire.endTransmittion(); //送信の実行
+//   Wire.endTransmission(); //送信0の実行
 //   Wire.requestFrom(address, 1); //slaveアドレスを再定義。レジスタアドレスから1バイトのデータを読み込む
 //   sensdata = Wire.read();
 //   Serial.println(sensdata);
-//   delay(500)
-// }
+//   delay(500);
+//}
 
 void loop(){
   Wire.beginTransmission(address); //電流データの焼き鳥
-  Wire.write(0x07);
-  Wire.endTransmittion();
+  Wire.write(7);
+  Wire.endTransmission();
   Wire.requestFrom(address, 1);
   current = Wire.read();
 
   Wire.beginTransmission(address); //電圧データの焼き鳥
-  Wire.write(0x04);
-  Wire.endTransmittion();
+  Wire.write(4);
+  Wire.endTransmission();
   Wire.requestFrom(address, 1);
   voltage = Wire.read();
 
-  Serial.print(current);
+//  Serial.print(current);
   Serial.println(voltage);
   
   delay(500);
