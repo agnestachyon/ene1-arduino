490841c9ea21078591b2b187df21182335aee25c
agnestachyon <yuu.hori.15.17@gmail.com>
2022-06-04 18:02:47 +0900

    デバイスIDと電圧の取得

--- vameter/vameter.ino
+++ vameter/vameter.ino
@@ -5,35 +5,36 @@ int sensdata; //センサから受け取ったデータを格納
 int current;
 int voltage;
 
-void setup(){
+void setup() {
   Wire.begin();
   Serial.begin(115200);
 }
 
-// void loop(){ //通信・デバイスの固有ID確認用
-//   Wire.beginTransmission(address); //通信の開始。slave側のアドレスを定義する
-//   Wire.endTransmission(); //送信0の実行
-//   Wire.requestFrom(address, 1); //slaveアドレスを再定義。レジスタアドレスから1バイトのデータを読み込む
-//   sensdata = Wire.read();
-//   Serial.println(sensdata);
-//   delay(500);
+//void loop(){ //通信・デバイスの固有ID確認用
+//  Wire.beginTransmission(address); //通信の開始。slave側のアドレスを定義する
+//  Wire.write(63);
+//  Wire.endTransmission(); //送信0の実行
+//  Wire.requestFrom(address, 2); //slaveアドレスを再定義。レジスタアドレスから2バイトのデータを読み込む
+//  sensdata = Wire.read();
+//  Serial.println(sensdata);
+//  delay(500);
 //}
 
-void loop(){
-  Wire.beginTransmission(address); //電流データの焼き鳥
-  Wire.write(7);
-  Wire.endTransmission();
-  Wire.requestFrom(address, 1);
-  current = Wire.read();
-
-  Wire.beginTransmission(address); //電圧データの焼き鳥
+void loop() {
+  Wire.beginTransmission(address); //電流データ
   Wire.write(4);
   Wire.endTransmission();
-  Wire.requestFrom(address, 1);
+  Wire.requestFrom(address, 3);
+  current = Wire.read();
+
+  Wire.beginTransmission(address); //電圧データ
+  Wire.write(5);
+  Wire.endTransmission();
+  Wire.requestFrom(address, 3);
   voltage = Wire.read();
 
-//  Serial.print(current);
+  Serial.println(current);
   Serial.println(voltage);
-  
+
   delay(500);
 }
