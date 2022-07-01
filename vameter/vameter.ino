62a15b0ec180d0f385ab3fb517672e999ce88e9f
agnestachyon <yuu.hori.15.17@gmail.com>
2022-07-02 03:27:52 +0900

    kairyoudekitetaraiikedo

--- vameter/vameter.ino
+++ vameter/vameter.ino
@@ -1,9 +1,8 @@
 #include <Wire.h> //I2C取り扱うためのライブラリ
-#define address 64//センサのアドレス
+#define addr 64//センサのアドレス
 
-int sensdata; //センサから受け取ったデータを格納
-int current;
-int voltage;
+double sensdata; //センサから受け取ったデータを格納
+double curr, volt;
 
 void setup() {
   Wire.begin();
@@ -11,30 +10,42 @@ void setup() {
 }
 
 //void loop(){ //通信・デバイスの固有ID確認用
-//  Wire.beginTransmission(address); //通信の開始。slave側のアドレスを定義する
+//  Wire.beginTransmission(addr); //通信の開始。slave側のアドレスを定義する
 //  Wire.write(63);
 //  Wire.endTransmission(); //送信0の実行
-//  Wire.requestFrom(address, 2); //slaveアドレスを再定義。レジスタアドレスから2バイトのデータを読み込む
+//  Wire.requestFrom(addr, 2); //slaveアドレスを再定義。レジスタアドレスから2バイトのデータを読み込む
 //  sensdata = Wire.read();
 //  Serial.println(sensdata);
 //  delay(500);
 //}
 
 void loop() {
-  Wire.beginTransmission(address); //電流データ
-  Wire.write(4);
-  Wire.endTransmission();
-  Wire.requestFrom(address, 3);
-  current = Wire.read();
+  curr = ina228_recv(addr, 4, 3);
 
-  Wire.beginTransmission(address); //電圧データ
-  Wire.write(5);
-  Wire.endTransmission();
-  Wire.requestFrom(address, 3);
-  voltage = Wire.read();
+//  Wire.beginTransmission(addr); //電圧データ
+//  Wire.write(5);
+//  Wire.endTransmission();
+//  Wire.requestFrom(addr, 3);
+//  volt = Wire.read();
 
-  Serial.println(current);
-  Serial.println(voltage);
+  Serial.println(curr);
+//  Serial.println(volt);
 
   delay(500);
+}
+
+double ina228_recv(unsigned int addr, unsigned char reg, unsigned char length){
+  long tmp;
+
+  Wire.beginTransmission(addr);
+  Wire.write(reg);
+  Wire.endTransmission();
+  Wire.requestFrom(addr, length);
+
+  tmp = Wire.read();
+  tmp = tmp << 12;
+  tmp += Wire.read() << 4;
+  tmp += Wire.read() >> 4;
+
+  return (double)tmp
 }
